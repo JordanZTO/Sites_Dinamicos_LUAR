1c9d0a5a5a089a63c67f771fee9a3a50d7e63b61
paulobomm <paulobomm@gmail.com>
2024-11-18 20:49:20 -0300

    siteconsertadolinks

--- js/functions.js
+++ js/functions.js
@@ -1,26 +1,5 @@
-/*function switch_menu() {
-    let dropdown_menu = document.getElementById("drop-down")
-    if (dropdown_menu) {
-        if (dropdown_menu.classList.contains("opacity-[0]")) {
-            dropdown_menu.classList.remove("opacity-[0]")
-        } else {
-            dropdown_menu.classList.add("opacity-[0]")
-        }
-    }
+/*
 
-}
-
-function link(link) {
-    window.location.href = link
-}
-
-function getBuy(link, e) {
-    console.log(link, e)
-    let doc = document.getElementById(e)
-    if (doc) {
-        window.location.href = link.replace("$quantidade", doc.value)
-    }
-}
 
 const url = "https://uzgfgx3m.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type%3D%3D%22product%22%5D"
 
@@ -161,3 +140,30 @@ async function loadRelatedProducts() {
 
 // Carregar produtos relacionados quando a página for carregada
 document.addEventListener("DOMContentLoaded", loadRelatedProducts);
+
+
+
+
+function switch_menu() {
+    let dropdown_menu = document.getElementById("drop-down")
+    if (dropdown_menu) {
+        if (dropdown_menu.classList.contains("opacity-[0]")) {
+            dropdown_menu.classList.remove("opacity-[0]")
+        } else {
+            dropdown_menu.classList.add("opacity-[0]")
+        }
+    }
+
+}
+
+function link(link) {
+    window.location.href = link
+}
+
+function getBuy(link, e) {
+    console.log(link, e)
+    let doc = document.getElementById(e)
+    if (doc) {
+        window.location.href = link.replace("$quantidade", doc.value)
+    }
+}
